e0ba9c9f7c3818048ec11ab7024c581787cdcb01
Cherepah <dotacontra@mailru>
2021-04-14 11:11:59 +0300

    base start

--- Source/src/js/main.js
+++ Source/src/js/main.js
@@ -1,3 +1,13 @@
-import './lib/lib';
+import $ from './lib/lib';
 
-$('.active').toggle();
+$('#first').on('click', () => {
+    $('div').eq(1).fadeToggle(800);
+});
+
+$('[data-count="second"]').on('click', () => {
+    $('div').eq(2).fadeToggle(2000);
+});
+
+$('button').eq(2).on('click', () => {
+    $('.w-500').fadeToggle(2000);
+});
